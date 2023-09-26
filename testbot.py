b7d92182c040d77435ca493bf3e3a7a5fa141d39
João Lopes (Keidi Francis) <65850554+Keidi16@users.noreply.github.com>
2023-09-26 13:27:48 +0100

    Delete testbot.py

--- testbot.py
+++
@@ -1,8 +0,0 @@
-from pybotnet import BotNet,TelegramEngine
-
-telegram_engine = TelegramEngine(token="token que bot gera", admin_chat_id="id do teu telegra")
-
-
-botnet = BotNet(telegram_engine) # 
-
-botnet.run()
